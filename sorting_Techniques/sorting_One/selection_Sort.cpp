c2228fcc12cae636962a32402b0119d402056737
Abbas Zaidi <abbasrazazaidi731@gmail.com>
2023-05-07 09:27:21 +0530

    Implemented selection sort algorithm

---
+++ sorting_Techniques/sorting_One/selection_Sort.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+void selection_sort(int arr[], int n)
+{
+    for (int i = 0; i <= n - 2; i++)
+    {
+        int mini = i;
+        for (int j = i; j <= n - 1; j++)
+        {
+            if (arr[j] < arr[mini])
+            {
+                mini = j;
+            }
+        }
+        int temp = arr[mini];
+        arr[mini] = arr[i];
+        arr[i] = temp;
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i{0}; i < n; i += 1)
+    {
+        cin >> arr[i];
+    }
+    selection_sort(arr, n);
+    for (int i = 0; i < n; i += 1)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
