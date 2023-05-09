47b5fa32afec69b782a9971a48856e61f1f1b294
Abbas Zaidi <abbasrazazaidi731@gmail.com>
2023-05-09 22:51:59 +0530

    Implemented insertion sort

---
+++ sorting_Techniques/sorting_One/insertion_Sort.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+void insertion_sort(int arr[], int n)
+{
+    for (int i = 0; i <= n - 1; i++)
+    {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j])
+        {
+            int temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i{0}; i < n; i += 1)
+    {
+        cin >> arr[i];
+    }
+    insertion_sort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
