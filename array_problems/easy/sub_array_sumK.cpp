b215d45dc0630a132f86aa73a4fb7eedec8661f0
AbbasZaidi11 <105381187+AbbasZaidi11@users.noreply.github.com>
2023-05-26 22:55:44 +0530

    program to find the subarray with sum K

---
+++ array_problems/easy/sub_array_sumK.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[] = {10, 5, 2, 7, 1, 9};
+    int k = 15;
+    vector<int> vec;
+    for (auto it : arr)
+    {
+        vec.push_back(it);
+    }
+    sort(vec.begin(), vec.end());
+    int sum;
+
+    return 0;
+}
