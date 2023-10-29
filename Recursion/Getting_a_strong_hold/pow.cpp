186ef8791aa4fce3ff353035eca30fa1d5318e34
Abbas Zaidi <105381187+AbbasZaidi11@users.noreply.github.com>
2023-10-29 21:49:32 +0530

    Add files via upload

---
+++ Recursion/Getting_a_strong_hold/pow.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    double myPow(double x, long n)
+    {
+        if (n == 0)
+            return 1;
+        if (n < 0)
+            return 1 / myPow(x, -n);
+        if (n & 1)
+            return x * myPow(x, n - 1);
+        return myPow(x * x, n / 2);
+    }
+};
+int main()
+{
+    return 0;
+}
