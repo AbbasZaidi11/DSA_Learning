0371d70a84127bc2f144c48978983263b0667533
Abbas Zaidi <105381187+AbbasZaidi11@users.noreply.github.com>
2023-11-03 23:12:11 +0530

    Create odd_even.cpp

---
+++ bit_manipulation/learn_bit_manipulation/odd_even.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+string oddEven(int N)
+{
+    // Write your code here.
+    if ((N & 1) == 0)
+    {
+        return "even";
+    }
+    return "odd";
+}
+int main()
+{
+    return 0;
+}
