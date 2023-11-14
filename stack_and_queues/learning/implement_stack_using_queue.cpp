e8be07bfc5fc0589f6b9facecf4d7b863bd78157
Abbas Zaidi <105381187+AbbasZaidi11@users.noreply.github.com>
2023-11-14 22:28:14 +0530

    Add files via upload

--- stack_and_queues/learning/implement_stack_using_queue.cpp
+++ stack_and_queues/learning/implement_stack_using_queue.cpp
@@ -1,5 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
+class MyStack
+{
+public:
+    queue<int> q;
+    int size = 0;
+    MyStack()
+    {
+    }
+
+    void push(int x)
+    {
+        size = size + 1;
+        int i = size - 1;
+        q.push(x);
+        while (i > 0)
+        {
+            int k = q.front();
+            q.pop();
+            q.push(k);
+            i--;
+        }
+    }
+
+    int pop()
+    {
+        int ans = q.front();
+        size = size - 1;
+        q.pop();
+        return ans;
+    }
+
+    int top()
+    {
+
+        return q.front();
+    }
+
+    bool empty()
+    {
+        return q.empty();
+    }
+};
+
 int main()
 {
     return 0;
