a0c4c1225f84564877ea6e3cce0a3489e0c62c3f
Abbas Zaidi <105381187+AbbasZaidi11@users.noreply.github.com>
2023-12-17 00:26:37 +0530

    Add files via upload

--- stack_and_queues/implementation/online_stock_span.cpp
+++ stack_and_queues/implementation/online_stock_span.cpp
@@ -3,38 +3,33 @@ using namespace std;
 class StockSpanner
 {
 public:
-    int size;
-    vector<int> vec;
+    stack<int> prices;
+    stack<int> span;
+
     StockSpanner()
     {
-        size = 0;
-        vec.resize(size);
     }
 
     int next(int price)
     {
-        vec.push_back(price);
-        size += 1;
-        int k = 0;
-        for (int i{size - 1}; i >= 0; i--)
+        if (prices.empty() == true || price < prices.top())
         {
-            if (vec[i] <= price)
-            {
-                k += 1;
-            }
-            else
-            {
-                break;
-            }
-        }
-        if (size == 1)
-        {
+            prices.push(price);
+            span.push(1);
             return 1;
         }
-        return k;
+        int ans = 1;
+        while (!prices.empty() && price >= prices.top())
+        {
+            ans = ans + span.top();
+            span.pop();
+            prices.pop();
+        }
+        prices.push(price);
+        span.push(ans);
+        return ans;
     }
 };
-
 int main()
 {
     return 0;
