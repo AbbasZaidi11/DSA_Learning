41115a82f17713e3aaddbe4ea8c1b4a6d2020a03
Abbas Zaidi <105381187+AbbasZaidi11@users.noreply.github.com>
2024-03-22 11:42:44 +0530

    Create inorder_traversals.cpp

---
+++ binary_trees/traversals/inorder_traversals.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+void inorderTraversal(Node* root,vector<int>&ans) {
+            if (root != nullptr) {
+                inorderTraversal(root->left,ans);
+                ans.push_back(root->data);
+                inorderTraversal(root->right,ans);
+            }
+        }
+    vector<int> inOrder(Node* root) {
+        // Your code here
+        vector<int>ans;
+        inorderTraversal(root,ans);
+        return ans;
+        
+    }
+int main(){
+  return 0;
+}
