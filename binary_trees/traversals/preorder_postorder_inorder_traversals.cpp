03367881389380e7ae5eb82edeb7dd7ecac2869b
Abbas Zaidi <105381187+AbbasZaidi11@users.noreply.github.com>
2024-04-08 14:20:18 +0530

    Update preorder_postorder_inorder_traversals.cpp

--- binary_trees/traversals/preorder_postorder_inorder_traversals.cpp
+++ binary_trees/traversals/preorder_postorder_inorder_traversals.cpp
@@ -1,34 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int>preInPostTraversal(TreeNode*root){
-stack<pair<TreeNode*,int>>st;
-st.push({root,1});
-vector<int>pre,in,post;
-  if(root==NULL)return ;
-  while(!st.empty()){
-    auto it=st.top();
-    st.pop();
-    if(it.second==1){
-      pre.push_back(it.first->val);
-      it.second++;
-      st.push(it);
-      if(it.first->left!=NULL){
-        st.push({it.first->left,1});
-      }
-  }
-    else if(it.second==2){
-      in.push_back(it.first->val);
-      it.second++;
-      st.push(it);
-      if(it.first->right!=NULL){
-        st.push({it.first->right,1});
-      }
+vector<vector<int>> getTreeTraversal(TreeNode *root){
+    // Write your code here.
+    vector<int>pre,ino,post;
+    vector<vector<int>>ans;
+    if(!root){
+        return ans;
     }
-    else{
-      post.push_back(it.first->val);
+    stack<pair<TreeNode*,int>>st;
+    st.push({root,1});
+    while(!st.empty()){
+        auto it=st.top();
+        st.pop();
+        if(it.second==1){
+            pre.push_back(it.first->data);
+            it.second=it.second+1;
+            st.push(it);
+            if(it.first->left){
+                st.push({it.first->left,1});
+
+            }
+        }
+        else if(it.second==2){
+            ino.push_back(it.first->data);
+            it.second=it.second+1;
+            st.push(it);
+            if(it.first->right){
+                st.push({it.first->right,1});
+
+            }
+        }
+        else{
+            post.push_back(it.first->data);
+        }
     }
-}
-  
+        ans.push_back(ino);
+
+    ans.push_back(pre);
+    ans.push_back(post);
+    return ans;
+    
 }
 int main(){
   return 0;
