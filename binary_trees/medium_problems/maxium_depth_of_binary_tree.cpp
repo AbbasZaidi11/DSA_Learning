72a0fca6dbbbf076f35cdc9adcef27e01a40fd19
Abbas Zaidi <105381187+AbbasZaidi11@users.noreply.github.com>
2024-04-08 22:04:21 +0530

    Create maxium_depth_of_binary_tree.cpp

---
+++ binary_trees/medium_problems/maxium_depth_of_binary_tree.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int maxDepth(TreeNode* root) {
+        vector<vector<int>>ans;
+        if(root==NULL)  return 0;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            int size=q.size();
+            vector<int>level;
+            for(int i{0};i<size;i++){
+                TreeNode*node=q.front();
+                q.pop();
+                if(node->left!=NULL){
+                    q.push(node->left);
+                }
+                if(node->right!=NULL){
+                    q.push(node->right);
+                }
+                level.push_back(node->val);
+            }
+            ans.push_back(level);
+        }
+        return ans.size();
+    }
+
+int main(){
+return 0;
+}
