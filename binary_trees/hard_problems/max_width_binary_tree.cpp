423d66a045a15e229c26f0e14cd8187eb762a626
Abbas Zaidi <105381187+AbbasZaidi11@users.noreply.github.com>
2024-05-11 19:06:50 +0530

    Create max_width_binary_tree.cpp

---
+++ binary_trees/hard_problems/max_width_binary_tree.cpp
@@ -0,0 +1,35 @@
+    int widthOfBinaryTree(TreeNode* root) {
+        if(!root){
+            return 0;
+        }
+        int ans=0;
+        queue<pair<TreeNode*,int>>q;
+        q.push({root,0});
+        while(!q.empty()){
+            int size=q.size();
+            int mmin=q.front().second;// finding the minimum at each level and subtracting from it to avoid overflow while indexing
+            
+            int first,last;
+            for(int i{0};i<size;i++){
+                int cur_id=q.front().second-mmin;
+                TreeNode*node=q.front().first;
+                q.pop();
+                if(i==0){
+                    first=cur_id;
+                }
+                if(i==size-1){
+                    last=cur_id;
+                }
+                if(node->left){
+                    q.push({node->left,(long long)cur_id*2+1});
+                 }
+                if(node->right){
+                    q.push({node->right,(long long)cur_id*2+2});
+                 }
+            }
+            ans=max(ans,last-first+1);
+        }
+        return ans;
+
+
+    }
