a5277654d041086ad75690c3b29ccba77206d278
Abbas Zaidi <105381187+AbbasZaidi11@users.noreply.github.com>
2024-07-18 22:42:29 +0530

    Create stickler_thief.cpp

---
+++ dynamic_programming/stickler_thief.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum money the thief can get.
+    int FindMaxSum(int arr[], int n)
+    {
+        // Your code here
+        int prev2=0;
+        int prev1=arr[0];
+        for(int i{1};i<n;i++){
+            //taking 
+            int take=arr[i]+prev2;
+            //not taking 
+            int not_take=prev1;
+            int curr=max(take,not_take);
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    //taking total testcases
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    //taking number of houses
+		int n;
+		cin>>n;
+		int a[n];
+		
+		//inserting money of each house in the array
+		for(int i=0;i<n;++i)
+			cin>>a[i];
+		Solution ob;
+		//calling function FindMaxSum()
+		cout<<ob.FindMaxSum(a,n)<<endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
